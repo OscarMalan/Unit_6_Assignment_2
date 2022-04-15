fc0e9d3ee210a5e00c32cf6fadbe05c06194829a
OscarMalan <Oscarmalan@icloud.com>
2022-04-15 23:39:48 +0100

    Adding dark mode

---
+++ style/Dark_Mode.js
@@ -0,0 +1,7 @@
+/**
+ * This function allows the button for dark mode to work, while some of the other js code that there will be wont be necesarry on all pages
+ */
+function Dark_Mode_Button() {
+    let element = document.body;
+    element.classList.toggle("Dark_Mode");
+ }
