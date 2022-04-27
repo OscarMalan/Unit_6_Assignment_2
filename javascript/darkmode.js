4d9e3156662f157a08ddaf488391865aa2d30953
Oscar Malan <oscarmalan@icloud.com>
2022-04-27 21:33:08 +0100

    dark mode stuff

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -1,7 +1,7 @@
 // This js file is for dark mode, it needs to be imported into every page.
-function myFunction() {
+let p_Color = "#1d2225";
+function DarkMode() {
     let element = document.body;
     element.classList.toggle("Dark_Mode");
-    document.getElementById("p").style.color = "blue";
-    document.getElementsByTagName("p1").style.color = "red";
+    p_Color = "aliceblue"
  }
