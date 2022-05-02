fe9617c2419303a8b2e290ab6f10b6d844f1bd28
OscarMalan <Oscarmalan@icloud.com>
2022-05-02 09:42:43 +0100

    Commenting

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -1,6 +1,10 @@
 // This js file is for dark mode, it needs to be imported into every page.
+// Makes a variable stating whether dark mode is on or off
 let Dark_Mode = false;
+// Pulls in the cookie for darkmode
 let Dark_Mode_Cookie = document.cookie;
+// Does a test to see if the dark mode cookie exists, or if they have got a cookie what cookie it is
+// also changes the Dark_Mode so that it is correct.
 if (Dark_Mode_Cookie == "Dark_Mode?=light")
 {
     document.getElementById("Mode").href = "../style/light_mode.css";
@@ -15,6 +19,7 @@ else
 {
     document.getElementById("Mode").href = "../style/light_mode.css";
 }
+// Function that the button uses to change dark mode, whilst also updating the cookie
 function DarkMode() {
     if (Dark_Mode == false)
     {
